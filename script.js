a5fa9817375e74d132646266007baa2b3c1b161e
jrej <jrejoire@hotmail.fr>
2019-10-25 13:24:55 +0200

    Js animation test

---
+++ script.js
@@ -0,0 +1,10 @@
+var tl = new TimelineMax();
+const controller = new ScrollMagic.Controller();
+
+tl.from('#about', 1, {x:200, opacity: 0});
+
+const scene2 = new ScrollMagic.Scene({
+  triggerElement: "#about"
+})
+  .setTween(tl)
+		.addTo(controller);
