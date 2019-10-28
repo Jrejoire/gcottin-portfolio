c27e79abedbba8d25c374fb1a179d3cb8fe022fc
Jrej <jrejoire@hotmail.fr>
2019-10-28 06:41:58 +0100

    animation nav + skills

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 var tl = new TimelineMax();
+var tl2 = new TimelineMax();
+var tl3 = new TimelineMax();
+
 const controller = new ScrollMagic.Controller();
 
-tl.from('#about', 1, {x:200, opacity: 0});
+tl.from('li', 1, { x: 10, opacity: 0, ease: Power2.easeInOut });
+tl2.from('#who', 1, { x: 100, opacity: 0, ease: Power2.easeInOut });
+tl2.from('#why', 1, { x: 100, opacity: 0, ease: Power2.easeInOut }, '-=0.1');
+tl3.from('.project-tile', 1, { opacity: 0, ease: Power2.easeInOut });
 
-const scene2 = new ScrollMagic.Scene({
+const scene = new ScrollMagic.Scene({
   triggerElement: "#about"
 })
-  .setTween(tl)
+  .setTween(tl2)
+  .addTo(controller);
+
+  const scene2 = new ScrollMagic.Scene({
+    triggerElement: "#projects"
+  })
+    .setTween(tl3)
     .addTo(controller);
